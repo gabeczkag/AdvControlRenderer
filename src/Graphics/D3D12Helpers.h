1a44daa8d85a6b0b000acd7d93439c2a5392eec7
gabeczkag <karczmarczykgrzegorz100@gmail.com>
2026-09-19 09:22:33 +0200

    Add complete Adv. Control renderer source

---
+++ src/Graphics/D3D12Helpers.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <d3d12.h>
+
+struct CD3DX12_RESOURCE_BARRIER
+{
+    static D3D12_RESOURCE_BARRIER Transition(
+        ID3D12Resource* resource,
+        D3D12_RESOURCE_STATES before,
+        D3D12_RESOURCE_STATES after)
+    {
+        D3D12_RESOURCE_BARRIER barrier{};
+        barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+        barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+        barrier.Transition.pResource = resource;
+        barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+        barrier.Transition.StateBefore = before;
+        barrier.Transition.StateAfter = after;
+        return barrier;
+    }
+};
